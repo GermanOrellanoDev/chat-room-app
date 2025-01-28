94fe556dfb0ea60a7d7f047ea9ca89a086d70980
German Orellano <gorellano.dev@gmail.com>
2025-01-28 17:34:55 -0300

    style/responsive-ok

--- client/src/components/Typing/Typing.tsx
+++ client/src/components/Typing/Typing.tsx
@@ -31,9 +31,9 @@ const Typing: React.FC = () => {
   };
 
   return (
-    <div className="relative flex items-center w-full gap-2 p-4">
+    <div className="relative flex items-center w-full gap-1 p-2 md:gap-2 md:p-4">
       <input
-        className="flex-1 rounded-full border border-purple-200 text-gray-500 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all duration-200 ease-in-out"
+        className="flex-1 rounded-full border border-purple-200 text-base sm:text-lg md:text-xl text-gray-500 px-2 py-1 md:px-4 md:py-2 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all duration-200 ease-in-out"
         type="text"
         value={message}
         onChange={(e) => setMessage(e.target.value)}
@@ -45,21 +45,26 @@ const Typing: React.FC = () => {
         aria-label="Send message"
         onClick={sendMessage}
       >
-        <FaPaperPlane className="w-5 h-5 mr-1" />
+        <FaPaperPlane className="w-3 h-3 mr-0.5 md:w-5 md:h-5 md:mr-1" />
       </button>
       <button
         className="bg-gradient-to-r from-purple-500 to-pink-500 text-white rounded-full p-3 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-200 ease-in-out"
         onClick={() => setShowEmoji(!showEmoji)}
       >
         {showEmoji ? (
-          <IoMdCloseCircle className="w-5 h-5" />
+          <IoMdCloseCircle className="w-3 h-3 md:w-5 md:h-5" />
         ) : (
-          <MdEmojiEmotions className="w-5 h-5" />
+          <MdEmojiEmotions className="w-3 h-3 md:w-5 md:h-5" />
         )}
       </button>
       {showEmoji && (
-        <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 z-50">
-          <Picker height={300} onEmojiClick={handleEmojiSelect} />
+        <div className="absolute bottom-full left-1/2 md:right-3 transform -translate-x-1/2 md:-translate-x-2 mb-2 z-50">
+          <Picker
+            className="h-80"
+            height={300}
+            width={"auto"}
+            onEmojiClick={handleEmojiSelect}
+          />
         </div>
       )}
     </div>
